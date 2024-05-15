bd5581a11b0e0d38211739ad28774c05a1b1b3df
AREEBASALEEM123 <166440229+AREEBASALEEM123@users.noreply.github.com>
2024-05-16 01:06:50 +0500

    CODE WITH AREEBA

---
+++ main.ts
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"
+
+console.log("\n\t Welcome-code-with-areeba-\n")
+
+let answer=Math.floor(Math.random()*5)
+let randomNumber=await inquirer. prompt([
+    {
+        name:"value",
+        type:"number",
+        message:"Enter your guess number"
+    }
+])
+if (randomNumber.value===answer){
+    console.log("congratulations! you win")
+}
+else{
+    console.log("sorry! you lose,try again")
+}
